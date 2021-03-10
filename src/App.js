a290b7bf6edb4eb97e57f2000a1327aee2e11924
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-03-10 18:45:56 -0300

    Exercis one - contextApi-refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import ContextCar from './context/ContextCar';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      cars: {
+        ...this.state.cars,
+        [car]: side,
+      },
+    });
+  };
+
+  render() {
+    const context = {
+      ...this.state,
+      moveCar: this.moveCar,
+    }
+    return (
+      <ContextCar.Provider value={context}>
+        <Cars />
+      </ContextCar.Provider>
+    );
+  }
 }
 
 export default App;
